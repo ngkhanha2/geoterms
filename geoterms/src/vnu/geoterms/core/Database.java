7dac6e6ab4b9ac306e006c6b225dfb75ab5f33aa
Khanh Nguyen <nguyenkhanh.cantho.2@gmail.com>
2014-10-29 22:57:20 +0700

    Begin building core

---
+++ geoterms/src/vnu/geoterms/core/Database.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package vnu.geoterms.core;
+
+/**
+ *
+ * @author Khanh
+ */
+public class Database {
+    
+}
---
+++ geoterms/src/vnu/geoterms/core/rbtree/RBTree.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vnu.geoterms.core.rbtree;
+
+import vnu.geoterms.core.interfaces.*;
+
+/**
+ *
+ * @author Khanh
+ */
+public class RBTree implements IStructure {
+
+    private IComparator comparator;
+
+    @Override
+    public void insert(Object item) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void remove(Object item) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void search(Object item) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public IIterator getIterator() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
